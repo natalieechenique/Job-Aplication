ac4028407037e47666434c3eff3ccd28fd4b1e8d
natalieechenique <natalie.terapias@gmail.com>
2020-08-20 09:45:48 -0400

    Maquetacion y rutas

---
+++ portafolio/src/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export const Error = () => (
+<div>
+    <h2>Error 404</h2>
+    <p>La página solicitada no existe</p>
+
+</div>
+
+)
---
+++ portafolio/src/Proyects.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+export const Proyects = () => (
+<div>
+    <h2>Proyectos</h2>
+    <br></br>
+    <p>Orbeyogawheel // Owner e-commerce</p>
+    <p>Didactic Covid-19 (Gamification-integration) // For Hootie Hoo</p>
+    <p>Get your mdLinks // mdLinks</p>
+    <p>Burger Queen // Burger Queen</p>
+    <p>Face Wars // Social Network</p>
+    <p>Harry Potter Beginners // Data Lovers</p>
+    <p>Easy Pay // Card Validation</p>
+    <p>Trivia Ciencias // Trivia</p>
+
+</div>
+
+)
